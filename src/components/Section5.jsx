c66c6a1169288a59133c6af5b84e410d8d192a7a
John Paul San Diego <johnpaulsandiego@Johns-MacBook-Pro.local>
2024-09-04 13:39:12 +0100

    Minor tweaks to css

--- src/components/Section5.jsx
+++ src/components/Section5.jsx
@@ -3,16 +3,17 @@ import StyledLink from './StyledLink';
 
 const Section5 = () => {
     return (
-        <div className="bg-neutral-900 relative font-work-sans py-16 px-4" id="contactSection">
+        <div className="bg-neutral-900 relative font-work-sans py-20 px-4" id="contactSection">
             
             <div className="flex flex-col gap-4 w-full max-w-[100rem] mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-white">
-                    <div className="flex flex-col items-center md:items-start px-4">
+            
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-white">
+                    <div className="flex flex-col items-start px-4">
                         <blockquote className="text-md font-normal italic font-bitter text-neutral-300 leading-relaxed">
                             Together, we can improve our future through coding.
                         </blockquote>
                     </div>
-                    <div className="flex flex-col items-center md:items-start px-4">
+                    <div className="flex flex-col items-start px-4">
                         <h1 className="text-xl font-semibold">Stay Connected</h1>
                         <StyledLink href="https://github.com/jd-paul">
                             https://github.com/jd-paul
@@ -21,7 +22,7 @@ const Section5 = () => {
                             https://www.linkedin.com/in/paul-san-diego/
                         </StyledLink>
                     </div>
-                    <div className="flex flex-col items-center md:items-start px-4">
+                    <div className="flex flex-col items-start px-4">
                         <h1 className="text-xl font-semibold">Location</h1>
                         <p className="text-md font-normal font-bitter text-neutral-300 leading-normal mt-2">
                             Great Britain, London
@@ -34,7 +35,7 @@ const Section5 = () => {
                 </div>
             </div>
 
-            <div className="text-neutral-300 text-center mt-10 font-bitter px-4">
+            <div className="text-neutral-300 items-start md:text-center mt-10 font-bitter px-4">
                 © 2024 Paul San Diego · United Kingdom · Designed by me
             </div>
         </div>
--- src/components/StyledLink.jsx
+++ src/components/StyledLink.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 const StyledLink = ({ href, children }) => {
   return (
-    <p className="text-md font-normal font-bitter text-neutral-300 leading-normal block tracking-normal mt-2 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none">
+    <p className="text-md font-normal font-bitter text-custom-accent_alt leading-normal block tracking-normal mt-2 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none">
       <a
         href={href}
-        className="relative inline-block text-neutral-300 hover:text-white"
-        style={{
-          textDecoration: 'none',
-        }}
+        className="relative inline-block underline transition-colors duration-200 ease-in-out hover:no-underline hover:text-neutral-300"
       >
         {children}
       </a>
